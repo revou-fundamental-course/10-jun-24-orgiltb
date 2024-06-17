8896520573b7814cbfaeddce3383fa625bb92a4c
orgiltb <ahmadwardani97@gmail.com>
2024-06-17 18:01:28 +0700

    calculating

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 /* JS */
+
+function getCelsius(x, y, z) {
+  var x = document.getElementById("celsiustext").value;
+  var y = (x * 9) / 5 + 32;
+  var z = document.getElementById("fahrenheitText").value = y;
+}
