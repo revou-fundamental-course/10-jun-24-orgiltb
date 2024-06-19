440e518d44c9dcee734d29144a011aad87e12e9a
orgiltb <ahmadwardani97@gmail.com>
2024-06-19 18:40:21 +0700

    swapping temp

--- js/script.js
+++ js/script.js
@@ -1,7 +1,46 @@
 /* JS */
+var celsiusCheck = true;
 
-function getCelsius(x, y, z) {
-  var x = document.getElementById("celsiustext").value;
-  var y = (x * 9) / 5 + 32;
-  var z = document.getElementById("fahrenheitText").value = y;
+function doConvert() {
+  if (celsiusCheck === true) {
+    var x = document.getElementById("firstText").value;
+    var y = (document.getElementById("secondText").value = (x * 9) / 5 + 32);
+    document.getElementById(
+      "convertText"
+    ).value = `(${x}\xB0C × 9/5) + 32 = ${y}\xB0F`;
+  } else {
+    var x = document.getElementById("firstText").value;
+    var y = (document.getElementById("secondText").value =
+      ((x - 32) * 5) / 9).toFixed(2);
+    document.getElementById(
+      "convertText"
+    ).value = `(${x}\xB0F - 32) * 5 / 9 = ${y}\xB0C`;
+  }
+}
+
+function doReverse() {
+  if (celsiusCheck === true) {
+    document.getElementById("firstText").placeholder = "32";
+    document.getElementById("secondText").placeholder = "0";
+    firstGroup.innerText = "Fahrenheit \xB0F";
+    secondGroup.innerText = "Celsius \xB0C";
+    celsiusCheck = false;
+    console.log(celsiusCheck);
+    document.getElementById("convertText").value = "(\xB0F - 32) × 5/9 = \xB0C";
+  } else {
+    document.getElementById("firstText").placeholder = "0";
+    document.getElementById("secondText").placeholder = "32";
+    firstGroup.innerText = "Celsius \xB0C";
+    secondGroup.innerText = "Fahrenheit \xB0F";
+    celsiusCheck = true;
+    console.log(celsiusCheck);
+    document.getElementById("convertText").value = "(\xB0C × 9/5) + 32 = \xB0F";
+  }
+  doConvert.x = document.getElementById("firstText").value = "";
+  doConvert.y = document.getElementById("secondText").value = "";
+}
+
+function doReset() {
+  doConvert.x = document.getElementById("firstText").value = "";
+  doConvert.y = document.getElementById("secondText").value = "";
 }
